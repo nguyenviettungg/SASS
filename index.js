87f8a291e1003da9ee19269f75ab3a703259f36a
Tungdeptrai <nguyentung696969@gmail.com>
2021-08-07 18:39:09 +0700

    add navigation button

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const { performance } = require("perf_hooks");
-const nemo = ["nemo"];
-const large = new Array(100).fill("nemo");
+// const { performance } = require("perf_hooks");
+// const nemo = ["nemo"];
+// const large = new Array(100).fill("nemo");
 
-function findNemo(array) {
-  let t0 = performance.now();
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === "nemo") {
-      console.log("found it");
-    }
-  }
-  let t1 = performance.now();
-  console.log("call to find nemo took " + (t1 - t0) + " milliseconds");
-}
-findNemo(large);
+// function findNemo(array) {
+//   let t0 = performance.now();
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === "nemo") {
+//       console.log("found it");
+//     }
+//   }
+//   let t1 = performance.now();
+//   console.log("call to find nemo took " + (t1 - t0) + " milliseconds");
+// }
+// findNemo(large);
+
